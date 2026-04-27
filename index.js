8e9e1dbf9f2d1611e1268a78d4a40ac94b229339
Vaex Mungaji <vaexmungajijr@gmail.com>
2026-04-27 13:02:08 +0100

    Atualização no método de contagem

--- index.js
+++ index.js
@@ -3,18 +3,17 @@ let countEl = document.getElementById("count-el")
 let btnCounter=document.getElementById('increment-btn')
 let count = 0
 
-btnCounter.onclick=function()
+btnCounter.addEventListener('click',function()
 {
     const increment= setInterval(() =>{
     count += 1
     countEl.textContent = count
     },1000)
-}
-
+})
 function save() {
+    clearInterval(increment)
     let countStr = count + " - "
     saveEl.textContent += countStr
     countEl.textContent = 0
     count = 0
-    clearInterval(increment)
 }
