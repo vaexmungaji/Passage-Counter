16d6a3d400e8740be524e4b806ad974fca1c0c65
Vaex Mungaji <vaexmungajijr@gmail.com>
2026-04-27 12:56:33 +0100

    Atualizei o método de contagem

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
+let btnCounter=document.getElementById('increment-btn')
 let count = 0
 
-const increment= setInterval(() =>{
+btnCounter.onclick=function()
+{
+    const increment= setInterval(() =>{
     count += 1
     countEl.textContent = count
     },1000)
-
+}
 
 function save() {
     let countStr = count + " - "
