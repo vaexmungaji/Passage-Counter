1d3bd32f34420e07632de5109ea07825e0c54d93
Vaex Mungaji <vaexmungajijr@gmail.com>
2026-04-27 12:49:02 +0100

    Modifiquei o método de contagem usando setInterval

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let count = 0
 
-function increment() {
+const increment= setInterval(() =>{
     count += 1
     countEl.textContent = count
-}
+    },1000)
+
 
 function save() {
     let countStr = count + " - "
     saveEl.textContent += countStr
     countEl.textContent = 0
     count = 0
+    clearInterval(increment)
 }
